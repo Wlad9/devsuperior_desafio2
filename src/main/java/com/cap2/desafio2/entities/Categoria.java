98655c8c0a7c2f3249c05f9c98a6ed2adc1e7719
wlad9 <zav8b9@gmail.com>
2023-07-29 21:12:16 -0300

    Entidades prontas e mapeadas. Relacionamento completo

---
+++ src/main/java/com/cap2/desafio2/entities/Categoria.java
@@ -0,0 +1,43 @@
+package com.cap2.desafio2.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+//  Relacionamento Categoria;Atividade :Muitos-para-Um
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividades = new ArrayList<>();
+}
---
+++ src/main/java/com/cap2/desafio2/entities/Participante.java
@@ -0,0 +1,58 @@
+package com.cap2.desafio2.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+
+    public Participante() {
+    }
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+//  Relacionamento Participante/Atividade : Muitos-para-Muitos
+
+    @ManyToMany(mappedBy = "participantes")
+    Set<Atividade> atividades = new HashSet<>();
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+}
